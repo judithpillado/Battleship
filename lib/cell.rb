dd406a953bec920ecbd22df9f1b85dfb8dd703b7
Arique Aguilar <arique1104@gmail.com>
2020-05-13 17:12:45 -0500

    Fire_upon test and passing code

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -14,5 +15,15 @@ class Cell
     @ship = ship
   end
 
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if !empty?
+      ship.hit
+    end
+    @fired_upon = true
+  end
 
 end
