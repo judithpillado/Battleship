80079c63341296f2b4ebe520af54daa9cd4da4f8
Judith Pillado <judith.pillado@gmail.com>
2020-05-14 22:54:19 -0500

    Add board cells hash

--- test/board_test.rb
+++ test/board_test.rb
@@ -13,5 +13,11 @@ class BoardTest < Minitest::Test
   def test_it_exists
     assert_instance_of Board, @board
   end
-  
+
+  def test_board_cells
+    assert_equal ["A1", "A2", "A3", "A4", "B1"], @board.cells.keys[0..4]
+    assert_equal 16, @board.cells.count
+    assert_instance_of Hash, @board.cells
+  end
+
 end
