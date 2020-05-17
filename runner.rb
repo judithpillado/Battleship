068ba5e0bfb8d2213ef9e392c68decb41d667d55
Arique Aguilar <arique1104@gmail.com>
2020-05-17 17:22:41 -0500

    Runner file created for Battleship Iteration 4

---
+++ runner.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+p "Welcome to BATTLESHIP"
+p "Enter p to play.  Enter q to quit"
+response = "> "
+
+response
